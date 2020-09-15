42abb34e1ae9f991dd0d2a1f9b1cac10604c9a11
Seo Jiye <seo-jiye@naver.com>
2020-09-16 00:59:21 +0900

    Updates

---
+++ precache-manifest.c1c132ac93947ff2642916584212b3a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d883ad1b9cf74635e4afb80d04240851",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "2e32627592c0a31f8841",
+    "url": "/movie_app/static/css/main.f6fb30ed.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e32627592c0a31f8841",
+    "url": "/movie_app/static/js/main.ded14eb9.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
